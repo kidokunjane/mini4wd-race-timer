d31acde841add9d509d4ac9f0e2c91276187c025
kidokunjane <yoyobaka@gmail.com>
2025-08-13 21:53:12 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Minimal service worker for offline shell caching */
-const CACHE_NAME = 'mini4wd-race-timer-v2';
+const CACHE_NAME = 'mini4wd-race-timer-v3';
 const APP_SHELL = [
   '/',
   '/index.html',
